c93ee9151e389415937c9346314603cfda981e7f
TymykO <152806676+TymykO@users.noreply.github.com>
2025-01-31 19:29:46 +0100

    add simple front

--- bavovna/main/views.py
+++ bavovna/main/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/about.html')
 
 def contact(request):
     return render(request, 'main/contact.html')
